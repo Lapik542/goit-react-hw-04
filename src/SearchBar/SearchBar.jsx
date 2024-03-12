a1ce810c6e1ae32c71d430f6aa6d7648ff1c5a31
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-03-12 19:46:32 +0200

    main

--- src/SearchBar/SearchBar.jsx
+++ src/SearchBar/SearchBar.jsx
@@ -1,19 +1,25 @@
-import { Form, Formik, Field } from 'formik';
-import css from './SearchBar.module.css'
+import { Form, Formik, Field, ErrorMessage } from 'formik'; // Додайте імпорт для ErrorMessage
+import * as Yup from 'yup';
+import css from './SearchBar.module.css';
+
+const validationSchema = Yup.object().shape({
+   query: Yup.string().required('Поле пошуку не може бути порожнім')
+});
 
 export const SearchBar = ({onSearch}) => {
    return (
       <header className={css.headerBar}>
-         <Formik initialValues={{query: ''}} onSubmit={(values, actions) => {
+         <Formik initialValues={{query: ''}} validationSchema={validationSchema} onSubmit={(values, actions) => {
             console.log(values);
             onSearch(values.query);
             actions.resetForm();
          }}>
             <Form>
                <Field name='query' />
+               <button type='submit'>Search</button>
+               <ErrorMessage name="query" component="div" className="error" /> {/* Додано ErrorMessage */}
             </Form>
-            <button type='submit'>Search</button>
          </Formik>
       </header>
    );
-}
+};
